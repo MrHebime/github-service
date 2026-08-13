d4724ad022511cbb86be6926cc28691fcaefca2d
John Doran <doranjayster@gmail.com>
2026-08-13 12:58:02 -0700

    fix: Fix GitHubUser avatar_url and created_at

--- src/main/java/com/example/github_service/model/GitHubUser.java
+++ src/main/java/com/example/github_service/model/GitHubUser.java
@@ -1,5 +1,6 @@
 package com.example.github_service.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 public class GitHubUser {
     String login;
     String name;
+    @JsonProperty("avatar_url")
     String avatarUrl;
     String location;
     String email;
     String url;
+    @JsonProperty("created_at")
     String createdAt;
 }
